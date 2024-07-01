41f30c3fe9e31a181cdb789aaf60d039a00890f2
Shri-Ram-P-A <pashriram30704@gmail.com>
2024-07-01 20:59:25 +0530

    feat:Add doubly Linke List

---
+++ Doubly-linked-list.py
@@ -0,0 +1,82 @@
+class Node:
+    def __init__(self,data):
+        self.pre = None
+        self.data = data
+        self.next = None
+class Dll:
+    def __init__(self):
+        self.head=None
+
+    def beg(self,data):
+        node = Node(data)
+        t=self.head
+        self.head = node
+        t.pre = node
+        node.next = t
+
+    def end(self,data):
+        node = Node(data)
+        cur = self.head
+        while cur.next:
+            cur = cur.next
+        t = cur.next
+        cur.next = node
+        node.pre = t
+
+    def pos(self,p,data):
+        node = Node(data)
+        cur = self.head
+        for i in range(1,p-1):
+            cur = cur.next
+        t = cur.next
+        node.pre = t
+        cur.next = node
+        node.next = cur.next.pre
+        cur.next.pre = node
+
+    def append(self,data):
+        node = Node(data)
+        if self.head is None:
+            self.head = node
+            return
+        cur = self.head
+        while cur.next:
+            cur = cur.next
+        cur.next = node
+        node.pre = cur
+
+    def dbeg(self):
+        self.head = self.head.next
+
+    def dend(self):
+        cur = self.head
+        while cur.next.next:
+            cur = cur.next
+        cur.next = None
+
+    def dpos(self,p):
+        cur = self.head
+        for i in range(1, p - 1):
+            cur = cur.next
+        t = cur
+        cur.next = cur.next.next
+        cur.next.pre = t
+
+    def disp(self):
+        if self.head is None:
+            print("No value present in Linked List")
+            return
+        cur = self.head
+        while cur:
+            print(cur.data,end="<->")
+            cur = cur.next
+        print("None")
+
+dl = Dll()
+for i in range(1,6):
+    dl.append(i)
+dl.beg(3)
+dl.pos(3,100)
+dl.dbeg()
+dl.dpos(3)
+dl.disp()
